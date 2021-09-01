6fbe02836acecbe07fc2aaea7060de5ffd329eb5
Franco Parente <fparente14@frba.utn.edu.ar>
2021-09-01 19:54:51 -0300

    subo apuntes y creo archivos scanner .c y .h

---
+++ TP 1/main.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+//Notas de clase
+/*
+el scanner es un programa que es llamado.
+el parser usa tokens para armar un arbol.
+el main llama a gettoken.
+no hay que hacer un main que llame un token que haga todo, eso esta mal.
+hay que llamar un gettoken para que lea varios tokens.
+hay q compartir un buffer.
+*/
+
+//ejemplo de enum:
+/*
+enum week{Mon, Tue, Wed, Thur, Fri, Sat, Sun};
+  
+int main()
+{
+    enum week day;
+    day = Wed;
+    printf("%d",day);
+    return 0;
+} 
+*/
+
+enum token {FDT=EOF, CAD, SEP=","};
+
